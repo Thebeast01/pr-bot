056624374f517d0cb6c6b997029a2d6f9d67132b
thebeast01 <mohammadsaif0847@gmail.com>
2025-04-26 14:09:22 +0530

    Initialized Bot

---
+++ ai_pr_summerizer.ts
@@ -0,0 +1,18 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import * as dotenv from "dotenv";
+dotenv.config();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+export const summarizeDiff = async (diff: string): Promise<string> => {
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  const prompt = `You are a helpful assistant that writes clean PR descriptions based on git diffs.Here is the diff:\`\`\`diff${diff}\`\`\`Write a summary of the changes in bullet points like a pull request description.
+Only include what the changes do (not the raw code).
+Also try to keep it short and concise.
+`;
+
+  const result = await model.generateContent(prompt)
+  const response = result.response
+  const summary = response.text();
+  console.log("Summary of changes:", summary);
+  return summary || 'No Summary generated';
+}
+
---
+++ generate-pr-description.ts
@@ -0,0 +1,49 @@
+import { Octokit } from "@octokit/rest";
+import * as dotenv from "dotenv";
+import { summarizeDiff } from "./ai_pr_summerizer";
+dotenv.config();
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+const generatePrDescription = async (owner: string, repo: string, prNumber: number) => {
+  try {
+    const { data: files } = await octokit.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: prNumber,
+    })
+    let description = `## Changes\n\n`;
+    for (const file of files) {
+      if (file.status === "added") {
+        description += `- Added ${file.filename}\n`;
+
+      } else if (file.status === "modified") {
+        description += `✏️ - Modified ${file.filename}\n`;
+      }
+      else if (file.status === "removed") {
+        description += `🗑️ - Removed ${file.filename}\n`;
+      }
+    }
+
+    const response = await summarizeDiff(description)
+    await octokit.pulls.update({
+      owner,
+      repo,
+      pull_number: prNumber,
+      body: response,
+    })
+    console.log("PR description updated successfully!");
+  } catch (e) {
+    console.error("Error generating PR description:", e);
+    return;
+  }
+}
+const main = async () => {
+  const [owner, repo] = (process.env.GITHUB_REPOSITORY || "").split("/");
+  const pull_number = parseInt(process.env.PR_NUMBER || "0", 10);
+  if (!owner || !repo || !pull_number) {
+    throw new Error("Missing owner, repo, or pull_number!");
+  }
+  await generatePrDescription(owner, repo, pull_number);
+}
+main().catch((e) => {
+  console.error("Error in main function:", e);
+});
