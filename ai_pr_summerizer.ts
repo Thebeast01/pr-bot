405a4e550cf37c6dfb62af758b38a5391ddd1094
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-13 14:00:55 +0530

    Added

--- ai_pr_summerizer.ts
+++ ai_pr_summerizer.ts
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import * as dotenv from "dotenv";
+console.log("Hello WOrld")
 dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 export const summarizeDiff = async (diff: string): Promise<string> => {
